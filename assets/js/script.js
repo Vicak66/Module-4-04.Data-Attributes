10aea22c92d12048c814bb6809412b4c9bb7380c
vicak <vicak614@gmail.com>
2024-09-03 13:57:01 +0630

    change attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ var x = document.querySelector(".container");
 x.addEventListener("click", function (y) {
     var z = y.target;
     if (z.matches(".box")) {
-        var a = z.getAttribute("data-state");
+        var a = z.getAttribute("data-change");
 
         if (a === "hidden") {
-            z.textContent = z.dataset.number;
-            z.dataset.state = "visible";
+            z.textContent = z.dataset.num;
+            z.dataset.change = "visible";
         } else {
             z.textContent = "";
-            z.setAttribute("data-state", "hidden");
+            z.setAttribute("data-change", "hidden");
         }
     }
 });
